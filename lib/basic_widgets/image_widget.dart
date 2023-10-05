cc7a21555ffb9b0a1d5b3ce1ed8487084f23d217
Jauhar Maulana <jauharmaulana4546@gmail.com>
2023-10-05 10:14:58 +0700

    Finish Praktikum

--- lib/basic_widgets/image_widget.dart
+++ lib/basic_widgets/image_widget.dart
@@ -6,7 +6,7 @@ class MyImageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Image(
-      image: AssetImage("logo_polinema.png")
+      image: ResizeImage(AssetImage("logo_polinema.png"),width: 400,height: 400)
     );
   }
 }
